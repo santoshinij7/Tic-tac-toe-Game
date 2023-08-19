97c6bca71eef91914b22a3e406ea00a454e3117d
santoshinij7 <santoshinij7@gmail.com>
2023-08-19 11:11:58 +0530

    added js

---
+++ gamelogic.js
@@ -0,0 +1,67 @@
+let playerText = document.getElementById('playerText');
+let restartBtn = document.getElementById('restartBtn');
+let boxes = Array.form (document.getElementByClassName('box'));
+
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning_blocks')
+
+const O_TEXT = 'O';
+const X_TEXT = 'X';
+let currentPlayer =  X_TEXT
+let spaces = Array(9).fill(null)
+
+const startGame = () => {
+    boxes.forEach(box => addEventListener('click', boxClicked))
+}
+function boxClicked(e){
+    const id = e.target.id 
+
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+
+        if(playerHasWon() !== false){
+            playerText = `${currentPlayer} has won!`
+            let winning_blocks = playerHasWon()
+
+            console.log(winning_blocks)
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    }
+}
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function playerHasWon(){
+    for (const condition of winningCombos){
+        let [a,b,c] = condition
+
+        if (spaces[a] && (spaces[a] == spaces [b] && spaces [a] && spaces[c])){
+            return[a,b,c]
+        }
+    }
+}
+
+
+restartBtn.addEventListener('click',restart)
+function restart(){
+    spaces.fill(null)
+    boxes.forEach(box => {
+        box.innerText = ''
+    })
+    playerText = 'Tic Tac Toe'
+
+
+    currentPlayer = X_TEXT
+}
+startGame()
